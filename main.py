c2df8bca57e094c6e75626e2d275eeb095af2937
Negin Firouzian <negfir@gmail.com>
2020-10-02 00:23:07 +0330

    0.789967

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import sys
 import os
 import time
 
+from nltk import tokenize
+from nltk.stem import *
+from nltk.tokenize import word_tokenize
 import sklearn
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -34,6 +37,10 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk import word_tokenize,sent_tokenize
 from sklearn.naive_bayes import MultinomialNB
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize
+from nltk.stem.porter import PorterStemmer
+
 if __name__ == '__main__':
     #Raw data is converted to seperate files for each comment, inorder to read them easily
     # file_neg = open('./rt-polaritydata/rt-polaritydata/rt-polarity.neg')
@@ -56,6 +63,7 @@ if __name__ == '__main__':
     #     f2.write(comment)
     #     cnt += 1
 
+
     data = []
     data_labels = []
     with open("./rt-polaritydata/rt-polaritydata/rt-polarity.neg") as f:
@@ -69,29 +77,106 @@ if __name__ == '__main__':
             data.append(i.strip("\n"))
             data_labels.append('neg')
 
-    # x_train, x_test, y_train, y_test = train_test_split(data, data_labels, test_size=0.20, random_state=12)
-    # movieVectorizer = CountVectorizer(min_df=2,tokenizer=nltk.word_tokenize,max_df = 1.0, analyzer='word', lowercase=False)
+    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.neg") as f:
     #
-    # features = movieVectorizer.fit_transform(data)
-    # x_test_counts=docs_test_counts = movieVectorizer.transform(x_test)
-    # print(movieVectorizer.vocabulary_.get("the"))
-    # features_nd = features.toarray()  # for easy usage
-    # print(features.shape)
-    # print(y_train.shape)
+    #     for i in f:
+    #         p=""
+    #         words = tokenize.word_tokenize(i.strip("\n"))
+    #         for w in words:
+    #             p=p+ps.stem(w)+" "
+    #         data.append(p)
+    #         #print(i)
+    #         data_labels.append('pos')
     #
-    #
-    #
-    # clf = MultinomialNB()
-    # clf.fit(features, y_train)
-    #
-    # y_pred = clf.predict(x_test_counts)
-    # print(sklearn.metrics.accuracy_score(y_test, y_pred))
+    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.pos") as f:
+    #     for i in f:
+    #         n=""
+    #         words = tokenize.word_tokenize(i.strip("\n"))
+    #         for w in words:
+    #             p=p+ps.stem(w)+" "
+    #         data.append(n)
+    #         data_labels.append('neg')
 
+    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.neg") as f:
+    #
+    #     for i in f:
+    #         p=""
+    #         words = tokenize.word_tokenize(i.strip("\n"))
+    #         for w in words:
+    #             p=p+lemmatizer.lemmatize(w)+" "
+    #         data.append(p)
+    #         #print(i)
+    #         data_labels.append('pos')
+    #
+    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.pos") as f:
+    #     for i in f:
+    #         n=""
+    #         words = tokenize.word_tokenize(i.strip("\n"))
+    #         for w in words:
+    #             p=p+lemmatizer.lemmatize(w)+" "
+    #         data.append(n)
+    #         data_labels.append('neg')
 
+    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.neg") as f:
+    #
+    #     for i in f:
+    #         p=""
+    #         words = tokenize.word_tokenize(i.strip("\n"))
+    #         for w in i.split(' '):
+    #             p=p+w+" "
+    #         data.append(p)
+    #         #print(i)
+    #         data_labels.append('pos')
+    #
+    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.pos") as f:
+    #     for i in f:
+    #         n=""
+    #         words = tokenize.word_tokenize(i.strip("\n"))
+    #         for w in i.split(' '):
+    #             p=p+w+" "
+    #         data.append(n)
+    #         data_labels.append('neg')
 
-    vectorizer = CountVectorizer(min_df=2,max_df = 0.7,
+    data_list = ['the gamers playing games', 'higher scores', 'sports']
+    print(data[8])
+    # words = tokenize.word_tokenize(data[8])
+    # for w in words:
+    #     print(ps.stem(w))
+
+    # from nltk.stem.snowball import SnowballStemmer
+    #
+    # stemmer = SnowballStemmer('english')
+    # new_corpus = [' '.join([stemmer.stem(word) for word in data_list.split(' ')])
+    #               for text in words]
+    print(data[89])
+
+    import string
+
+    stemmer = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+
+    def lemma_tokens(tokens, lemmatizer):
+        lemma = []
+        for item in tokens:
+            lemma.append(lemmatizer.lemmatize(item))
+        return lemma
+
+    def stem_tokens(tokens, stemmer):
+        stemmed = []
+        for item in tokens:
+            stemmed.append(stemmer.stem(item))
+        return stemmed
+
+    def tokenize(text):
+        text = "".join([ch for ch in text if (ch not in string.punctuation and ch not in string.digits)])
+        tokens = nltk.word_tokenize(text)
+        #stems = stem_tokens(tokens, stemmer)
+        lemma = lemma_tokens(tokens, lemmatizer)
+        return lemma
+
+    vectorizer = CountVectorizer(min_df=2,max_df = 0.4,
         analyzer='word',
-        lowercase=False
+        lowercase=False,tokenizer=tokenize
     )
     features = vectorizer.fit_transform(
         data
@@ -100,8 +185,8 @@ if __name__ == '__main__':
     TFvectorizer = TfidfTransformer()
     TFfeatures = TFvectorizer.fit_transform(features)
 
-    features_nd = features.toarray()  # for easy usage
-
+    features_nd = TFfeatures.toarray()  # for easy usage
+    #print(vectorizer.vocabulary_)
     X_train, X_test, y_train, y_test = train_test_split(
         features_nd,
         data_labels,
