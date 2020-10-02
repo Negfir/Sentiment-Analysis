4924268671facaaa00dadcc263d933ee5e7e78dc
Negin Firouzian <negfir@gmail.com>
2020-10-02 11:20:19 +0330

    Parameter tuning

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import sys
 import os
 import time
 
+import numpy as np
 from nltk import tokenize
 from nltk.stem import *
 from nltk.tokenize import word_tokenize
@@ -17,18 +18,19 @@ import sklearn
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
+from sklearn.svm import *
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import load_files
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn import metrics, svm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import confusion_matrix
 import nltk
 import nltk
+from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
@@ -36,32 +38,15 @@ from sklearn.model_selection import train_test_split
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk import word_tokenize,sent_tokenize
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import *
+from sklearn.tree import *
 from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 
 if __name__ == '__main__':
     #Raw data is converted to seperate files for each comment, inorder to read them easily
-    # file_neg = open('./rt-polaritydata/rt-polaritydata/rt-polarity.neg')
-    # file_pos = open('./rt-polaritydata/rt-polaritydata/rt-polarity.pos')
-    # contents_Pos= file_pos.read()
-    # comments_Pos=contents_Pos.splitlines()
-    # contents_Neg= file_neg.read()
-    # conmments_Neg=contents_Neg.splitlines()
-    # cnt=0
-    # for comment in comments_Pos:
-    #     print(comment, "\n")
-    #     f1 = open("comments/pos/comment-pos%s.txt" % cnt, "w")
-    #     f1.write(comment)
-    #     cnt += 1
-    # cnt=0
-    #
-    # for comment in conmments_Neg:
-    #     print(comment, "\n")
-    #     f2 = open("comments/neg/comment-neg%s.txt" % cnt, "w")
-    #     f2.write(comment)
-    #     cnt += 1
+
 
 
     data = []
@@ -77,77 +62,9 @@ if __name__ == '__main__':
             data.append(i.strip("\n"))
             data_labels.append('neg')
 
-    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.neg") as f:
-    #
-    #     for i in f:
-    #         p=""
-    #         words = tokenize.word_tokenize(i.strip("\n"))
-    #         for w in words:
-    #             p=p+ps.stem(w)+" "
-    #         data.append(p)
-    #         #print(i)
-    #         data_labels.append('pos')
-    #
-    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.pos") as f:
-    #     for i in f:
-    #         n=""
-    #         words = tokenize.word_tokenize(i.strip("\n"))
-    #         for w in words:
-    #             p=p+ps.stem(w)+" "
-    #         data.append(n)
-    #         data_labels.append('neg')
 
-    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.neg") as f:
-    #
-    #     for i in f:
-    #         p=""
-    #         words = tokenize.word_tokenize(i.strip("\n"))
-    #         for w in words:
-    #             p=p+lemmatizer.lemmatize(w)+" "
-    #         data.append(p)
-    #         #print(i)
-    #         data_labels.append('pos')
-    #
-    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.pos") as f:
-    #     for i in f:
-    #         n=""
-    #         words = tokenize.word_tokenize(i.strip("\n"))
-    #         for w in words:
-    #             p=p+lemmatizer.lemmatize(w)+" "
-    #         data.append(n)
-    #         data_labels.append('neg')
-
-    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.neg") as f:
-    #
-    #     for i in f:
-    #         p=""
-    #         words = tokenize.word_tokenize(i.strip("\n"))
-    #         for w in i.split(' '):
-    #             p=p+w+" "
-    #         data.append(p)
-    #         #print(i)
-    #         data_labels.append('pos')
-    #
-    # with open("./rt-polaritydata/rt-polaritydata/rt-polarity.pos") as f:
-    #     for i in f:
-    #         n=""
-    #         words = tokenize.word_tokenize(i.strip("\n"))
-    #         for w in i.split(' '):
-    #             p=p+w+" "
-    #         data.append(n)
-    #         data_labels.append('neg')
-
-    data_list = ['the gamers playing games', 'higher scores', 'sports']
     print(data[8])
-    # words = tokenize.word_tokenize(data[8])
-    # for w in words:
-    #     print(ps.stem(w))
 
-    # from nltk.stem.snowball import SnowballStemmer
-    #
-    # stemmer = SnowballStemmer('english')
-    # new_corpus = [' '.join([stemmer.stem(word) for word in data_list.split(' ')])
-    #               for text in words]
     print(data[89])
 
     import string
@@ -168,58 +85,104 @@ if __name__ == '__main__':
         return stemmed
 
     def tokenize(text):
+        stops = set(stopwords.words("english"))
         text = "".join([ch for ch in text if (ch not in string.punctuation and ch not in string.digits)])
         tokens = nltk.word_tokenize(text)
         #stems = stem_tokens(tokens, stemmer)
+
         lemma = lemma_tokens(tokens, lemmatizer)
+        #posTag = nltk.pos_tag(lemma)
         return lemma
 
-    vectorizer = CountVectorizer(min_df=2,max_df = 0.4,
-        analyzer='word',
-        lowercase=False,tokenizer=tokenize
-    )
-    features = vectorizer.fit_transform(
-        data
-    )
 
-    TFvectorizer = TfidfTransformer()
-    TFfeatures = TFvectorizer.fit_transform(features)
-
-    features_nd = TFfeatures.toarray()  # for easy usage
-    #print(vectorizer.vocabulary_)
     X_train, X_test, y_train, y_test = train_test_split(
-        features_nd,
+        data,
         data_labels,
         train_size=0.80,
         random_state=1234)
 
+    vectorizer = CountVectorizer(min_df=2,max_df = 0.5,
+        analyzer='word',
+        lowercase=False,tokenizer=tokenize
+    )
+    features_train = vectorizer.fit_transform(
+        X_train
+    )
+    features_test = vectorizer.transform(
+        X_test
+    )
+    TFvectorizer = TfidfTransformer()
+    TFfeatures_train = TFvectorizer.fit_transform(features_train)
+    TFfeatures_test = TFvectorizer.transform(features_test)
+
+  #  features_nd = TFfeatures.toarray()  # for easy usage
+    #print(vectorizer.vocabulary_)
+
+
     from sklearn.linear_model import LogisticRegression
 
     #log_model = LogisticRegression()
-    log_model = MultinomialNB()
+    #log_model = MultinomialNB(alpha=0.85,fit_prior=True, class_prior=None)
+    log_model = BernoulliNB(alpha=0.85)
+    #log_model = MLPClassifier(hidden_layer_sizes=(500,)) #0.73
+    #log_model = DecisionTreeClassifier() #0.63 very bad
+
     #log_model=svm.SVC(kernel='linear')
-    #log_model = RandomForestClassifier(n_estimators=200, random_state=0)
-    t0 = time.time()
-    log_model = log_model.fit(X=X_train, y=y_train)
-    t1 = time.time()
-    y_pred = log_model.predict(X_test)
-    t2 = time.time()
-    time_train = t1 - t0
-    time_predict = t2 - t1
-    # import random
+    #log_model=svm.LinearSVC(C=1.0, tol=0.0001, max_iter=1000, penalty='l2', loss='squared_hinge', dual=True, multi_class='ovr', fit_intercept=True, intercept_scaling=1)
+    #log_model = RandomForestClassifier(n_estimators=200, random_state=0) #0.71
+
+    tuned_parameters = [{'alpha': [0.0, 0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8,0.9]}]
+
+    scores = ['precision', 'recall']
+
+    for score in scores:
+        print("# Tuning hyper-parameters for %s" % score)
+        print()
+
+        clf = GridSearchCV(
+            BernoulliNB(), tuned_parameters, scoring='%s_macro' % score
+        )
+        clf.fit(TFfeatures_train, y_train)
+
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_)
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r"
+                  % (mean, std * 2, params))
+        print()
+
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(TFfeatures_test)
+        print(classification_report(y_true, y_pred))
+        print()
+
+
+    # t0 = time.time()
+    # log_model = log_model.fit(X=TFfeatures_train, y=y_train)
+    # t1 = time.time()
+    # y_pred = log_model.predict(TFfeatures_test)
+    # t2 = time.time()
+    # time_train = t1 - t0
+    # time_predict = t2 - t1
     #
-    # j = random.randint(0, len(X_test) - 7)
-    # for i in range(j, j + 7):
-    #     print(y_pred[0])
-    #     ind = features_nd.tolist().index(X_test[i].tolist())
-    #     print(data[ind].strip())
-
-    print("Training time: %fs; Prediction time: %fs" % (time_train, time_predict))
-
-    print(accuracy_score(y_test, y_pred))
-    print(metrics.classification_report(y_test, y_pred))
-    cm = confusion_matrix(y_test, y_pred)
-    print(cm)
+    #
+    #
+    # print("Training time: %fs; Prediction time: %fs" % (time_train, time_predict))
+    #
+    # print(accuracy_score(y_test, y_pred))
+    # print(metrics.classification_report(y_test, y_pred))
+    # cm = confusion_matrix(y_test, y_pred)
+    # print(cm)
 
 
 
